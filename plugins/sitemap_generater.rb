d6bb1edcdfc8187fd0451d86d9d77ccc91e5a60e
crhan <crhan123@gmail.com>
2012-09-07 00:00:50 +0800

    a

--- plugins/sitemap_generater.rb
+++ plugins/sitemap_generater.rb
@@ -10,27 +10,25 @@ class Ruhoh
         exclude_page = config['exclude'] rescue []
 
         sitemap = Nokogiri::XML::Builder.new(:encoding => 'UTF-8') do |xml|
-          xml.rss(:version => '2.0') {
-            xml.urlset(:xmlns => "http://www.sitemaps.org/schemas/sitemap/0.9") {
-              Ruhoh::DB.posts['chronological'].each do |post_id|
-                post = Ruhoh::DB.posts['dictionary'][post_id]
-                page.change(post_id)
-                xml.url {
-                  xml.loc_ "#{production_url}#{post['url']}"
-                  xml.lastmod_ (post['lastmod'] ? post['lastmod'] : post['date'])
-                  if !!post['priority']
-                    xml.priority_ post['priority']
-                  end
-                }
-              end #posts
+          xml.urlset(:xmlns => "http://www.sitemaps.org/schemas/sitemap/0.9") {
+            Ruhoh::DB.posts['chronological'].each do |post_id|
+              post = Ruhoh::DB.posts['dictionary'][post_id]
+              page.change(post_id)
+              xml.url {
+                xml.loc_ "#{production_url}#{post['url']}"
+                xml.lastmod_ (post['lastmod'] ? post['lastmod'] : post['date'])
+                if !!post['priority']
+                  xml.priority_ post['priority']
+                end
+              }
+            end #posts
 
-              Ruhoh::DB.pages.each do |name, meta|
-                next if exclude_page.include? meta['id']
-                xml.url {
-                  xml.loc_ "#{production_url}#{meta['url']}"
-                }
-              end #pages
-            }
+            Ruhoh::DB.pages.each do |name, meta|
+              next if exclude_page.include? meta['id']
+              xml.url {
+                xml.loc_ "#{production_url}#{meta['url']}"
+              }
+            end #pages
           }
         end
         File.open(File.join(target, sitemap_file_name), 'w') { |p| p.puts sitemap.to_xml }
